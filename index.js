ebe1f0f7895bcef746fae2271c2a08bb16f29f88
Ricard Torres <torres.rick@gmail.com>
2022-02-03 12:57:24 +0100

    Done

---
+++ index.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+let Parser = require('rss-parser');
+const templates = require('./templates.js');
+let parser = new Parser();
+const promises = [];
+const sources = JSON.parse(fs.readFileSync('sources.json'));
+
+// Create the requried folders
+fs.mkdir(`./dist`, () => {});
+
+function createFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (!err) {
+      console.log('File created: ' + fileName);
+    }
+  });
+}
+
+function itemTemplate(item) {
+  return `<li class="mb-1">
+    <a target="_blank" href="${item.link}" title="${item.title}">${item.title}</a>
+    <time datetime="${item.pubDate}" class="ps-2 small">${item.pubDate}</time>
+  </li>`
+}
+
+sources.sections.forEach((section) => {
+  section.items.forEach((item) => {
+    promises.push(parser.parseURL(item.url))
+  });
+});
+
+Promise.all(promises).then((feeds) => {
+  let output = ``;
+
+  feeds.forEach((feed) => {
+    output += `<section class="row">`;
+      output += `<div class="col">`;
+        output += `<h2 class="h3">${feed.title}</h2>`;
+        output += '<ul class="mb-4">';
+        output += feed.items.slice(0, 10).map(itemTemplate).join('');
+        output += '</ul>';
+      output += `</div>`;
+    output += `</section>`;
+  });
+
+  output = templates.document(output);
+
+  createFile('./dist/index.html', output)
+});
---
+++ templates.js
@@ -0,0 +1,37 @@
+module.exports.document = function (body) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daily digest</title>
+    <link type="text/css" rel="stylesheet" href="./styles.css" media="all">
+  </head>
+  <body>
+    <main>
+      <header class="bg-dark mb-4">
+        <nav class="container navbar navbar-dark">
+        <div class="container-fluid">
+          <h1 class="text-light h2 mb-0">Daily Digest</h1>
+          <a href="#" class="text-light" title="See repository on Github">Github</a>
+        </div>
+        </nav>
+      </header>
+      <div class="container mb-3">
+        <div class="row mb-3">
+          <div class="col">
+            <strong>Updated</strong>: ${new Date()}
+          </div>
+        </div>
+        ${body}
+      </div>
+      <footer class="bg-dark text-light p-4">
+        <div class="container text-center">
+          Made with <span class="pe-1">❤️</span> by <a class="text-light" href="https://ricard.blog/" title="Ricard Torres Blog">Ricard Torres</a>
+        </div>
+      </footer>
+    </main>
+  </body>
+  </html>`;
+}
