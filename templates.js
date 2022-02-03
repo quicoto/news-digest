3b8c8d4b667bc00a6a2643ff24869a5fc1e047d9
Ricard Torres <torres.rick@gmail.com>
2022-02-03 15:11:22 +0100

    Typo

--- templates.js
+++ templates.js
@@ -5,7 +5,7 @@ module.exports.document = function (body) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Daily digest</title>
+    <title>Daily Digest</title>
     <link type="text/css" rel="stylesheet" href="./styles.css" media="all">
   </head>
   <body>
